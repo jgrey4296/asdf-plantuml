8d82d9f1b557efc2a6dbffc57683a516829d0bf9
John Grey <johngrey4296@gmail.com>
2025-10-06 19:06:26 +0100

    [initial]:

--- contributing.md
+++ contributing.md
@@ -6,7 +6,7 @@ Testing Locally:
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
 # TODO: adapt this
-asdf plugin test blah https://github.com/jgrey4296/asdf-blah.git "blah --help"
+asdf plugin test plantuml https://github.com/jgrey4296/asdf-plantuml.git "plantuml -version"
 ```
 
 Tests are automatically run in GitHub Actions on push and PR.
--- lib/utils.bash
+++ lib/utils.bash
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+# asdf env vars:
+# ASDF_INSTALL_TYPE : version or ref
+# ASDF_INSTALL_VERSION : full version number or git ref
+# ASDF_INSTALL_PATH : where the tool should be
+# ASDF_CONCURRENCY : number of cores
+# ASDF_DOWNLOAD_PATH : where bin/download downloads to
+# ASDF_PLUGIN_PATH : where the plugin is installed
+# ASDF_PLUGIN_SOURCE_URL : url of the plugin
+# ASDF_PLUGIN_PREV_REF : previous git-ref of plugin
+# ASDF_PLUGIN_POST_REF : updated git-ref of plugin
+# ASDF_CMD_FILE : full path of file being sourced
+
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for <YOUR TOOL>.
-GH_REPO="<TOOL REPO>"
-TOOL_NAME="<YOUR TOOL>"
-TOOL_TEST="<TOOL CHECK>"
+GH_REPO="https://github.com/plantuml/plantuml"
+TOOL_NAME="plantuml"
+TOOL_TEST="plantuml -version"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -19,36 +30,30 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-sort_versions() {
+function sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
+function list_all_versions() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//'
+	# NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
-list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if <YOUR TOOL> has other means of determining installable versions.
-	list_github_tags
-}
-
-download_release() {
+function download_release() {
 	local version filename url
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for <YOUR TOOL>
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/releases/${version}/${filename}"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
-install_version() {
+function install_version() {
 	local install_type="$1"
 	local version="$2"
 	local install_path="${3%/bin}/bin"
@@ -59,9 +64,8 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$ASDF_INSTALL_VERSION.jar" "$install_path"
 
-		# TODO: Assert <YOUR TOOL> executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
