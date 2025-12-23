2a5ac7f7791c8b986b59ac7a6cc4807647f538ea
John Grey <johngrey4296@gmail.com>
2025-12-23 16:24:01 +0000

    [refactor]:

--- lib/utils.bash
+++ lib/utils.bash
@@ -25,7 +25,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if <YOUR TOOL> is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -39,45 +38,4 @@ function list_all_versions() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
 		sed 's/^v//'
-	# NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
-function download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
-	mkdir -p "$ASDF_DOWNLOAD_PATH"
-
-	url="$GH_REPO/releases/download/v${version}/${filename}"
-
-	echo "* Downloading $TOOL_NAME release $version..."
-	echo "* To: $ASDF_DOWNLOAD_PATH/$filename"
-	curl "${curl_opts[@]}" -o "$ASDF_DOWNLOAD_PATH/$filename" -C - "$url" || fail "Could not download $url"
-
-	test -f "$ASDF_DOWNLOAD_PATH/$filename" || fail "download file did not exist"
-}
-
-function install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
-	echo "* Installing to: $install_path"
-
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
-
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		java -jar "$install_path/$TOOL_NAME-$ASDF_INSTALL_VERSION.jar" -version || fail "Expected $install_path/$tool_cmd to be executable."
-
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
 }
